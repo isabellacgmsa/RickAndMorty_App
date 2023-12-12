a5832999b4fb8eb7f90e48a66bdd71045aae33ac
isabellacgmsa <isabellacgmsa1@gmail.com>
2023-12-12 11:28:46 -0300

    Pequenas mudanças visuais

--- src/components/CharacterList.tsx
+++ src/components/CharacterList.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, FlatList, Image, StyleSheet, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import axios from 'axios';
+import { Colors } from '../styles/styles';
 
 const CharacterList = () => {
   const [characters, setCharacters] = useState([]);
@@ -50,6 +51,11 @@ const CharacterList = () => {
 
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>Rick and Morty</Text>
+
+       <Text style={styles.introText}>
+        Bem-vindo à lista de personagens! Aqui estão alguns dos personagens incríveis de Rick and Morty. Para ver detalhes, basta clicar em um deles.
+      </Text>
       <FlatList
         data={characters}
         keyExtractor={(item) => item.id.toString()}
@@ -66,9 +72,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 16,
   },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 8,
+    marginTop: 50,
+    color: Colors.secondary
+  },
   characterItem: {
     marginBottom: 16,
     alignItems: 'center',
+  },
+  introText: {
+    fontSize: 16,
+    marginTop:20,
+    marginBottom: 16,
   },
   characterImage: {
     width: 100,
@@ -79,6 +97,8 @@ const styles = StyleSheet.create({
   characterName: {
     fontSize: 16,
     fontWeight: 'bold',
+    color: Colors.primary
+
   },
 });
 
